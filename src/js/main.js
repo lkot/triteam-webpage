ce9daf17df110f8e974e4c6ca11f97f4f26ccec0
Lukasz Kot <lukasz.kot@hotmail.com>
2021-10-26 18:47:16 +0200

    Achievements section styled + counter script implemented.

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,10 @@ const navBtn = document.querySelector('.hamburger');
 const mobileLogo = document.querySelector('.header__logo');
 const allNavLinks = document.querySelectorAll('.nav__link');
 
+const achievementList = document.querySelector('.achievements-list');
+const achievementNumber = document.querySelectorAll('.achievement-number');
+
+// Mobile menu toggle function.
 const handleNav = () => {
 	// Class 'is-acive' is a hamburger's built-in class
 	// It has to be toggled via JavaScript to do the animation.
@@ -31,3 +35,35 @@ const handleNav = () => {
 };
 
 navBtn.addEventListener('click', handleNav);
+
+// Achievements counter function.
+const options = {
+	rootMargin: '-250px',
+};
+
+const startCounter = (entry) => {
+	console.log(entry[0].isIntersecting);
+
+	if (entry[0].isIntersecting) {
+		achievementNumber.forEach((counter) => {
+			const updateCounter = () => {
+				const finalNumber = counter.getAttribute('data-number');
+				const value = parseInt(counter.textContent);
+
+				const speed = finalNumber / 200;
+
+				if (value < finalNumber) {
+					counter.textContent = `${Math.floor(value + speed)}`;
+					setTimeout(updateCounter, 1);
+				} else {
+					counter.textContent = finalNumber;
+				}
+			};
+
+			updateCounter();
+		});
+	}
+};
+
+const observer = new IntersectionObserver(startCounter, options);
+observer.observe(achievementList);
