87a2ef631ce15409bcfee09cb65ab341e131e6a6
Lukasz Kot <lukasz.kot@hotmail.com>
2021-10-26 14:11:03 +0200

    Slick carousel configured with team section.

---
+++ src/js/slicksettings.js
@@ -0,0 +1,23 @@
+$('.team-carousel').slick({
+	autoplay: true,
+	mobileFirst: true,
+
+	arrows: false,
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2,
+			},
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3,
+			},
+		},
+	],
+});
